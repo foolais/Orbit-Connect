2ac1e595b939e3cb9175d364ecf8347411d58551
Foolais <wahyu.esya17@gmail.com>
2025-12-13 14:31:20 +0700

    fix: signup code refactoring

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,13 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
-  connectDB();
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server is running on port " + PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("Failed to connect to the database:", error);
+    process.exit(1);
+  });
