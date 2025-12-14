13adb0837a184f02503b1eb7a24efc9bf35ff05b
Foolais <wahyu.esya17@gmail.com>
2025-12-14 20:58:49 +0700

    feat: update profile n check auth routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const _dirname = path.resolve();
@@ -12,6 +13,7 @@ const _dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
