f3cc1970a190d6dcb9c7b74be741e73826356571
Foolais <wahyu.esya17@gmail.com>
2026-01-27 15:04:58 +0700

    fix: cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,21 +12,7 @@ const app = express();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-
-    if (origin === ENV.CLIENT_URL) {
-      return callback(null, true);
-    }
-    return callback(null, false);
-  },
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
